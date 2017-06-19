ccbc56462b75ecfe94cc744d0168e98f24096fbd
Kristoffer Panahon <krp312@me.com>
2017-06-19 12:49:30 -0400

    planning stage

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-stateObject={
-	
+question1 = {
+    question: "What is the capital city of NY state?",
+    answers: ['NYC', 'Albany', 'Buffalo', 'Staten Island'],
+    correctAnswer: 1
 }
+
+stateObject={
+    view: //start, questions, results, final,
+	questions: [question1, question2, question3 ...],
+    score: 0,
+    currentQuestion: 1
+}
+
+// state modifier functions
+//user selectedAnswer is going to give us a number = index
+function scoreUpdater(state, userSelectedAnswer) {
+    if (userSelectedAnswer === state.questions[0].correctAnswer) {
+        state.score++;
+    }
+}
+
+// hit 'start quiz', loads up question 1
+// *then* user clicks an option
+console.log(scoreUpdater(stateObject, 3));
+
+// what's the current question?
+
+// function to update view
